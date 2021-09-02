3c714c447bcd8e8620e0fb90a26ea8cf0a626896
joelGit99 <87014970+joelGit99@users.noreply.github.com>
2021-09-02 17:27:32 +0200

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,15 @@
+var numero1 = 5;
+var numero2 = 8;
+
+if(numero2 > numero1) {
+    console.log("numero1 no es mayor que numero 2");
+}
+if(numero2 > 0) {
+    console.log("numero 2 es positivo");
+}
+if(numero2 < 0 || numero2 != 0) {
+    console.log("numero2 es negativo o diferente a cero");
+}
+if((numero1 + 1) < numero2) {
+    console.log("Incrementer en 1 el numero1 no lo hace mayor o igual a numero2");
+}
